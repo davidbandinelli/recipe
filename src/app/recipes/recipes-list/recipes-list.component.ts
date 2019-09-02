82a1e359d4308a0ed6c601d0d6011f8389d55cba
David Bandinelli <dbandinelli@unoinformatica.it>
2019-09-02 15:34:40 +0200

    ui

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [ 
+    // tslint:disable-next-line:max-line-length
+    new Recipe('A Test Recipe', 'this is a test', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/caponata-pasta_1.jpg'),
+    // tslint:disable-next-line:max-line-length
+    new Recipe('A Test Recipe', 'this is a test', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/caponata-pasta_1.jpg')
+  ];
 
   constructor() { }
 
